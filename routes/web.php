de8efbc09fd6644776babac69286c6b4ec0bde3d
Khuram Shahzad <khuram686@yahoo.com>
2023-05-20 13:05:33 +0500

    developing new

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hom', function () {
+Route::get('/ho', function () {
     return view('welcome');
 });
